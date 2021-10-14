8e9eacedeb639b41e8b3820441909acd8de0c4e4
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2021-10-14 14:29:51 -0700

    pre export document fixup

--- src/client/datascience/export/pythonScriptExporter.ts
+++ src/client/datascience/export/pythonScriptExporter.ts
@@ -1,16 +1,33 @@
-//import { Uri, workspace } from 'vscode';
-import { Uri } from 'vscode';
+import { NotebookCell, NotebookDocument, Uri, workspace } from 'vscode';
 import { injectable } from 'inversify';
 import { IPythonScriptExporter } from './types';
+import { traceError } from '../../common/logger';
 
 // IANHU: injectable best for this? Looks like it could just be a static function
 
-// The PythonScriptExporter is responsible only for
+// The PythonScriptExporter is responsible for turning a VS Code URI into a string of python text
 @injectable()
 export class PythonScriptExporter implements IPythonScriptExporter {
-    public export(_source: Uri): Promise<string> {
-        //const _doc = workspace.notebookDocuments.find((doc) => doc.uri === source);
+    public async export(source: Uri): Promise<string> {
+        const docs = workspace.notebookDocuments;
+        const doc = workspace.notebookDocuments.find((doc) => doc.uri === source);
 
-        return Promise.resolve('testing');
+        if (!doc) {
+            // IANHU: Allow unknown return here? Should not get this situation
+            traceError('Failed to find NotebookDocument for export');
+            return '';
+        }
+
+        return this.exportDocument(doc);
+    }
+
+    private exportDocument(document: NotebookDocument): string {
+        return document
+            .getCells()
+            .reduce((previousValue, currentValue) => previousValue + this.exportCell(currentValue), '');
+    }
+
+    private exportCell(_cell: NotebookCell): string {
+        return 'testing';
     }
 }
